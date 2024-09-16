08245dc120a452f912b0a2cd7c345ae50aa2c268
Joshua Matthews <joshua.matthews@patracorp.com>
2024-09-16 15:59:15 -0500

    fixed unit tests, have writing working properly

--- build.zig
+++ build.zig
@@ -38,10 +38,18 @@ pub fn build(b: *std.Build) void {
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    const lib_public_func_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const run_public_func_lib_unit_tests = b.addRunArtifact(lib_public_func_unit_tests);
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_lib_unit_tests.step);
+    test_step.dependOn(&run_public_func_lib_unit_tests.step);
 }
