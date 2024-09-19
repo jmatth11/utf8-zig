2e87831a3fc00a6043607550ead5742a4930fbe8
Joshua Matthews <jmatth93@gmail.com>
2024-09-19 17:05:04 -0500

    using u32 instead of keeping u8 array for simplicity

--- src/unicode.zig
+++ src/unicode.zig
@@ -8,7 +8,7 @@ const unicode_error = error{
 };
 
 pub const unicode = struct {
-    bytes: []u8,
+    bytes: []u32,
     pos: usize,
     alloc: std.mem.Allocator,
 
@@ -21,7 +21,7 @@ pub const unicode = struct {
 
     pub fn init(self: *unicode, n: usize, alloc: std.mem.Allocator) void {
         self.alloc = alloc;
-        self.bytes = alloc.alloc(u8, n);
+        self.bytes = alloc.alloc(u32, n);
         self.pos = 0;
     }
 
@@ -39,26 +39,25 @@ pub const unicode = struct {
     }
 
     pub fn write_raw_at(self: *unicode, point: u32, idx: usize) unicode_error!usize {
-        // TODO check if we need to push the next bytes to make room for this write
         const octet_t = utf8.octet_type_from_raw(point);
         if (octet_t == utf8.octet_type.OCT_INVALID) return unicode_error.invalid_format;
         const point_len = idx + octet_t.count();
         try self.resize(point_len);
-        const result = utf8.utf8_write_raw(self.bytes, self.bytes.len, self.pos, point);
+        self.bytes[idx] = point;
         if (point_len > self.pos) self.pos = point_len;
-        return result;
+        return 1;
     }
 
     pub fn write_at(self: *unicode, buf: []u8, idx: usize) unicode_error!usize {
-        // TODO check if we need to push the next bytes to make room for this write
         if (!utf8.utf8_verify(buf, buf.len)) return unicode_error.invalid_format;
         const write_len = idx + buf.len;
         self.resize(write_len) catch return unicode_error.alloc_error;
         var pos_idx = idx;
         var buf_idx = 0;
         while (pos_idx < buf.len) {
-            self.bytes[pos_idx] = buf[buf_idx];
-            buf_idx += 1;
+            const code_point = utf8.utf8_next(buf, buf.len, buf_idx);
+            self.bytes[pos_idx] = code_point.val;
+            buf_idx += code_point.type.count();
             pos_idx += 1;
         }
         if (write_len > self.pos) self.pos = write_len;
@@ -66,20 +65,12 @@ pub const unicode = struct {
     }
 
     pub fn len(self: *unicode) usize {
-        return utf8.utf8_len(self.bytes, self.bytes.len);
+        return self.bytes.len;
     }
 
     pub fn at(self: *unicode, idx: usize) unicode_error!u32 {
         const bytes_len = self.len();
         if (idx > bytes_len) return unicode_error.out_of_range;
-        var byte_idx: usize = 0;
-        var count: usize = 0;
-        while (count < idx) : (count += 1) {
-            const oct_t = utf8.get_oct_type(self.bytes[byte_idx]);
-            byte_idx += oct_t.count();
-        }
-        const point = utf8.utf8_next(self.bytes, self.bytes.len, byte_idx);
-        if (point.type == utf8.octet_type.OCT_INVALID) return unicode_error.invalid_format;
-        return point.val;
+        return self.bytes[idx];
     }
 };
