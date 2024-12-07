c614d00d0b35d3f931f24c5748458a4d2275f524
Joshua Matthews <jmatth93@gmail.com>
2024-12-06 23:15:47 -0600

    fleshed out more of the unicode structure

--- src/unicode.zig
+++ src/unicode.zig
@@ -1,11 +1,15 @@
 const std = @import("std");
 const utf8 = @import("utf8.zig");
 
-const unicode_error = error{
+pub const unicode_error = error{
     alloc_error,
     invalid_format,
     out_of_range,
+    not_supported,
 };
+
+pub const unicode_raw = u32;
+pub const unicode_utf8 = []u8;
 
 pub const unicode = struct {
     bytes: []u32,
@@ -42,62 +46,103 @@ pub const unicode = struct {
 
     pub fn write(comptime T: type, self: *unicode, buf: T) unicode_error!usize {
         return switch (T) {
-            []u8 => try self.write_at(buf, self.pos),
-            u32 => try self.write_raw_at(buf, self.pos),
+            unicode_utf8 => try self.write_at(buf, self.pos),
+            unicode_raw => try self.write_raw_at(buf, self.pos),
+            else => unicode_error.not_supported,
         };
     }
 
     pub fn write_at(comptime T: type, self: *unicode, buf: T, idx: usize) unicode_error!usize {
         return switch (T) {
-            []u8 => try self.write_at(buf, idx),
-            u32 => try self.write_raw_at(buf, idx),
+            unicode_utf8 => try self.write_at(buf, idx),
+            unicode_raw => try self.write_raw_at(buf, idx),
+            else => unicode_error.not_supported,
+        };
+    }
+
+    pub fn insert(comptime T: type, self: *unicode, buf: T) unicode_error!usize {
+        return switch (T) {
+            unicode_utf8 => try self.insert_at_code_points(buf, self.pos),
+            unicode_raw => try self.insert_raw_at(buf, self.pos),
+            else => unicode_error.not_supported,
+        };
+    }
+
+    pub fn insert_at(comptime T: type, self: *unicode, buf: T, idx: usize) unicode_error!usize {
+        return switch (T) {
+            unicode_utf8 => try self.insert_at_code_points(buf, idx),
+            unicode_raw => try self.insert_raw_at(buf, idx),
+            else => unicode_error.not_supported,
         };
     }
 
     pub fn insert_raw_at(self: *unicode, point: u32, idx: usize) unicode_error!usize {
-        const octet_t = utf8.octet_type_from_raw(point);
-        if (octet_t == utf8.octet_type.OCT_INVALID) return unicode_error.invalid_format;
-        const point_end_idx = idx + octet_t.count();
+        const octet_t = try self.validate_raw(point);
+        const point_end_idx = idx + 1;
         try move_range(idx, point_end_idx);
-        if (idx < self.pos) {}
+        return try self.write_octet_type_at(octet_t, idx);
+    }
+
+    pub fn insert_at_code_points(self: *unicode, buf: []u8, idx: usize) unicode_error!usize {
+        const buf_len = try self.validate_utf8_str(buf);
+        const write_len = idx + buf_len;
+        try self.move_range(idx, write_len);
+        return self.write_utf8_at(buf, idx);
     }
 
     fn write_raw_at(self: *unicode, point: u32, idx: usize) unicode_error!usize {
-        const octet_t = utf8.octet_type_from_raw(point);
-        if (octet_t == utf8.octet_type.OCT_INVALID) return unicode_error.invalid_format;
-        const point_end_idx = idx + octet_t.count();
+        const octet_t = try self.validate_raw(point);
+        return self.write_octet_type_at(octet_t, idx);
+    }
+
+    fn write_at_code_points(self: *unicode, buf: []u8, idx: usize) unicode_error!usize {
+        // verify the string is valid up front
+        const buf_len = try self.validate_utf8_str(buf);
+        const write_len = idx + buf_len;
+        self.resize(write_len) catch return unicode_error.alloc_error;
+        if (write_len > self.pos) self.pos = write_len;
+        return self.write_utf8_at(buf, idx);
+    }
+
+    fn write_octet_type_at(self: *unicode, point: utf8.octet_type, idx: usize) usize {
+        const point_end_idx = idx + point.count();
         try self.resize(point_end_idx);
         self.bytes[idx] = point;
         if (point_end_idx > self.pos) self.pos = point_end_idx;
         return 1;
     }
-
-    fn write_at_code_points(self: *unicode, buf: []u8, idx: usize) unicode_error!usize {
-        // verify the string is valid up front
-        if (!utf8.utf8_verify_str(buf, buf.len)) return unicode_error.invalid_format;
-        const write_len = idx + utf8.utf8_len(buf, buf.len);
-        self.resize(write_len) catch return unicode_error.alloc_error;
+    fn write_utf8_at(self: *unicode, buf: []u8, idx: usize) usize {
         var pos_idx = idx;
         var buf_idx = 0;
-        while (pos_idx < buf.len) {
+        while (buf_idx < buf.len) {
             const code_point = utf8.utf8_next(buf, buf.len, buf_idx);
             self.bytes[pos_idx] = code_point.val;
             buf_idx += code_point.type.count();
             pos_idx += 1;
         }
-        if (write_len > self.pos) self.pos = write_len;
         return buf_idx;
     }
 
     fn move_range(self: *unicode, start_idx: usize, end_idx: usize) unicode_error!void {
-        if (end_idx < start_idx) return unicode_error.out_of_range;
-        var end_pos = (self.pos + (end_idx - start_idx)) - 1;
+        if (end_idx < start_idx or start_idx > self.pos) return unicode_error.out_of_range;
+        var end_pos = (self.pos + (end_idx - start_idx));
+        self.pos = end_pos;
         try self.resize(end_pos);
-        var beg_pos = self.pos - 1;
+        if (start_idx >= self.pos) return;
+        var beg_pos = self.pos;
         while (beg_pos >= start_idx) {
             self.bytes[end_pos] = self.bytes[beg_pos];
             end_pos -= 1;
             beg_pos -= 1;
         }
+    }
+    inline fn validate_raw(n: u32) unicode_error!utf8.octet_type {
+        const octet_t = utf8.octet_type_from_raw(n);
+        if (octet_t == utf8.octet_type.OCT_INVALID) return unicode_error.invalid_format;
+        return octet_t;
+    }
+    inline fn validate_utf8_str(buf: []u8) unicode_error!usize {
+        if (!utf8.utf8_verify_str(buf, buf.len)) return unicode_error.invalid_format;
+        return utf8.utf8_len(buf, buf.len);
     }
 };
