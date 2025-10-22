48380f7e34d0fa96ffe9c820e34883699442faab
Joshua Matthews <jmatth93@gmail.com>
2025-10-22 11:03:40 -0500

    fixed build file for current zig version

--- build.zig
+++ build.zig
@@ -1,18 +1,7 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
     const linkage = b.option(std.builtin.LinkMode, "linkage", "Link mode for utf8-zig library") orelse .static;
@@ -66,24 +55,18 @@ pub fn build(b: *std.Build) void {
     weblib.bundle_compiler_rt = true;
     b.installArtifact(weblib);
 
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/unicode.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-    const lib_public_func_unit_tests = b.addTest(.{
+    const test_mod = b.createModule(.{
         .root_source_file = b.path("src/utf8.test.zig"),
-        .target = target,
         .optimize = optimize,
+        .target = target,
+    });
+    const lib_public_func_unit_tests = b.addTest(.{
+        .root_module = test_mod,
+        .name = "utf8-tests",
     });
 
     const run_public_func_lib_unit_tests = b.addRunArtifact(lib_public_func_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_public_func_lib_unit_tests.step);
 }
